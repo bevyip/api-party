ed7c27c1152f672a74ef54a2099e32cb2789a3a4
hyip8000 <hyip@purdue.edu>
2017-06-20 11:28:30 -0400

    Added route path to github

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, NavLink } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
@@ -7,11 +7,19 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h3>Ain't no party like an</h3>
-          <h1>API Party</h1>
+          <div className="App-headings">
+            <h3>Ain't no party like an</h3>
+            <h1>API Party</h1>
+          </div>
+          <ul className="nav-links">
+            <li>
+              <NavLink to="/github">Github API</NavLink>
+              </li>
+          </ul>
         </div>
         <Switch>
-          <Route render={() => <p>To get started, click one of the links above</p>} />
+           <Route path='/github' render={() => <h1>Github!</h1>} />
+           <Route render={() => <p>To get started, click one of the links above</p>} />
           </Switch>
       </div>
     );
