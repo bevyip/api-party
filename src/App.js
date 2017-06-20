171db8b86a87a87e9dd23f6d19c4d662484880db
hyip8000 <hyip@purdue.edu>
2017-06-20 16:51:55 -0400

    So i tried doing weather+googlemaps API but failed so bad so I tried this cute robot thing and I think i'm satisfied

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route, NavLink } from 'react-router-dom'
 import Github from './Github'
 import Nasa from './Nasa'
 import Homework from './Homework'
+import Robot from './RoboHash'
 import './App.css';
 
 class App extends Component {
@@ -22,6 +23,9 @@ class App extends Component {
               <NavLink to={'/nasa'}>NASA API</NavLink>
             </li>
             <li>
+              <NavLink to={'/robot'}>Robot API</NavLink>
+            </li>
+            <li>
               <NavLink to={'/homework'}>Homework</NavLink>
             </li>
           </ul>
@@ -29,6 +33,7 @@ class App extends Component {
         <Switch>
           <Route path='/github' component={Github} />
           <Route path='/nasa' component={Nasa} />
+          <Route path='/robot' component={Robot} />
           <Route path='/homework' component={Homework} />
           <Route render={() => <p>To get started, click one of the links above</p>} />
         </Switch>
